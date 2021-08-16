20b41f70c3a896045e42d622fedfdbce787378a8
argun8394 <argundoganali@gmail.com>
2021-08-16 23:58:44 +0300

    exampl

---
+++ 1-27 examp funct/app.js
@@ -0,0 +1,20 @@
+hesapA {
+    ad: "ali",
+    soyad: "argun",
+    hno: 123456,
+    bakiye: 2500,
+    ekhesap: 1000
+}
+
+hesapB {
+    ad: "Büşra",
+    soyad: "bolat",
+    hno: 123457,
+    bakiye: 2000,
+    ekhesap: 2000
+}
+
+function paraCek(hesap, miktar) {
+    console.log(`Merhaba ${hesap.ad} ${heap.soyad}`);
+
+}
