791e68b912e51ab48d6912d0cdf79c645077ef20
argun8394 <argundoganali@gmail.com>
2021-08-16 22:56:21 +0300

    funct exp

--- 1-26 funct declarations/app.js
+++ 1-26 funct declarations/app.js
@@ -1,8 +1,17 @@
-function sum(a, b) {
-    let c = a + b;
+// function sum(a, b) {
+//     let c = a + b;
+//     return c;
+// }
+
+// console.log(sum(10, 30));
+// console.log(sum(10, -25));
+// console.log(sum(120, 30));
+
+const sum = function(a, b) {
+    var c = a + b;
+
     return c;
+
 }
 
 console.log(sum(10, 30));
-console.log(sum(10, -25));
-console.log(sum(120, 30));
