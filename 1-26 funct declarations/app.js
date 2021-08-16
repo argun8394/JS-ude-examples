f4fc68e92a4dba329b00d02703c330f9975f0dc4
argun8394 <argundoganali@gmail.com>
2021-08-16 23:06:58 +0300

    args

--- 1-26 funct declarations/app.js
+++ 1-26 funct declarations/app.js
@@ -7,11 +7,37 @@
 // console.log(sum(10, -25));
 // console.log(sum(120, 30));
 
-const sum = function(a, b) {
-    var c = a + b;
+// const sum = function(a, b) {
+//     var c = a + b;
 
-    return c;
+//     return c;
+
+// }
+
+// console.log(sum(10, 30));
+
+// const sum = function(a, b) {
+//     if (typeof a === 'undefined') { a = 0 };
+//     if (typeof b === 'undefined') { b = 0 };
+
+//     var c = a + b;
+
+//     return c;
+
+// }
+
+// console.log(sum(10));
+
+function sumAll() {
+    var total = 0;
+    for (var i = 0; i < arguments.length; i++) {
+        total += arguments[i];
+
+    }
+
+
+    return total;
 
 }
 
-console.log(sum(10, 30));
+console.log(sumAll(10, 20, 30, 40, 50));
