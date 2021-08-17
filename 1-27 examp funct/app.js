5be9abfcfc36c5335f59a2ad4aa5586d305ed20c
argun8394 <argundoganali@gmail.com>
2021-08-17 23:36:58 +0300

    event bubbling

--- 1-27 examp funct/app.js
+++ 1-27 examp funct/app.js
@@ -1,20 +1,29 @@
-hesapA {
-    ad: "ali",
-    soyad: "argun",
-    hno: 123456,
-    bakiye: 2500,
-    ekhesap: 1000
+var hesapA = {
+    ad: 'Sena Turan',
+    hesapNo: '12345678',
+    bakiye: 2000,
+    ekHesap: 1000
 }
 
-hesapB {
-    ad: "Büşra",
-    soyad: "bolat",
-    hno: 123457,
-    bakiye: 2000,
-    ekhesap: 2000
+var hesapB = {
+    ad: 'Emel Turan',
+    hesapNo: '12356479',
+    bakiye: 3000,
+    ekHesap: 2000
 }
+
 
 function paraCek(hesap, miktar) {
-    console.log(`Merhaba ${hesap.ad} ${heap.soyad}`);
+    console.log(`Merhaba ${hesap.ad}`);
+    var toplam = bakiye + ekhesap;
 
-}
+    if (miktar > toplam) {
+        console.log("paranzı çekmek istermisiniz")
+    } else if (miktar < toplam) {
+        console.log("bakiye yetersiz")
+    }
+
+
+    paraCek(hesapA, 2000);
+    paraCek(hesapA, 1100);
+    paraCek(hesapB, 2000);
